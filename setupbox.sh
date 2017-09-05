19bb3b246a59d7eb3748d76bd47ff509183ccded
Ben Gonzales <bgonzales20001@gmail.com>
2017-09-05 11:39:33 -0500

    edit

--- setupbox.sh
+++ setupbox.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #install golang
-#sudo wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz >/dev/null 2>&1
+sudo wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz >/dev/null 2>&1
 sudo tar -zxvf go1.9.linux-amd64.tar.gz
 sudo mv go /usr/local
 
@@ -9,7 +9,7 @@ sudo mv go /usr/local
 sudo apt-get -y install git
 
 #install couchbase
-#curl -O http://packages.couchbase.com/releases/couchbase-release/couchbase-release-1.0-3-amd64.deb 
+curl -O http://packages.couchbase.com/releases/couchbase-release/couchbase-release-1.0-3-amd64.deb 
 sudo dpkg -i couchbase-release-1.0-3-amd64.deb
 sudo apt-get update
 sudo apt-get install -y --force-yes couchbase-server
